10fedfac1ea36a88044abc6ad6073a5497093393
Leo Klenner <leonardklenner@gmail.com>
2018-10-23 13:24:39 -0400

    Update CapsBot.py

--- CAPS/CapsBot.py
+++ CAPS/CapsBot.py
@@ -1,13 +1,21 @@
+""" 
+Terran bot that stays on 1base, builds two barracks and a factory
+Pushes into enemy base with min 20 marines and 5 cyclones
+Repeats that push with same numbers if unsuccessful at first
+Generally defeats hard AI in TvT, TvP and TvZ
+
+Bot made by LeoQK
+"""
 import random
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from sc2.constants import SCV, COMMANDCENTER, SUPPLYDEPOT, REFINERY, BARRACKS, MARINE, FACTORY, CYCLONE
 
-class CapsBot(sc2.BotAI):
-    async def on_step(self, iteration):
-        await self.distribute_workers()
-        await self.build_workers()
+class CapsBot(sc2.BotAI):  # CapsBot inherits methods from sc2.BotAI
+    async def on_step(self, iteration):  # at every step of the game on_step controls what we want to do at that step
+        await self.distribute_workers()  # defined in BotAI
+        await self.build_workers()   # all remaining methods defined by us
         await self.build_supplydepot()
         await self.build_refinery()
         await self.build_barracks()
@@ -15,68 +23,78 @@ class CapsBot(sc2.BotAI):
         await self.build_factory()
         await self.build_cyclones()
         await self.attack()
-
+    
+    # build workers (SCVs) through the COMMANDCENTER if we can afford a worker and the amount of workers is smaller than 80
     async def build_workers(self):
         for cc in self.units(COMMANDCENTER).ready.noqueue: #  cc has to be ready and not producing sth else
             if self.can_afford(SCV) and self.units(SCV).amount < 80:
                 await self.do(cc.train(SCV))
-
+                
+    # build SUPPLYDEPOT to increase supply if we have less than 8 supply left, are not already building one and can afford one
     async def build_supplydepot(self):
         if self.supply_left < 8 and not self.already_pending(SUPPLYDEPOT) and self.can_afford(SUPPLYDEPOT):
             if self.units(SUPPLYDEPOT).amount == 0:
-                near = self.units(COMMANDCENTER).first
+                near = self.units(COMMANDCENTER).first  # position first SUPPLYDEPOT near our first COMMANDCENTER 
                 await self.build(SUPPLYDEPOT, near)
             else:
-                near = self.units(SUPPLYDEPOT)[(self.units(SUPPLYDEPOT).amount - 1)]
+                near = self.units(SUPPLYDEPOT)[(self.units(SUPPLYDEPOT).amount - 1)] #  subsequent SUPPLYDEPOTs built near to previous one
                 await self.build(SUPPLYDEPOT, near)
-
+    
+    # build REFINERY to mine gas from vespene geysers if we can afford REFINERY
     async def build_refinery(self):
         vgs = self.state.vespene_geyser.closer_than(10.0, self.units(COMMANDCENTER).first)
         for vg in vgs:
             if not self.can_afford(REFINERY):
                 break
-            worker = self.select_build_worker(vg.position)
+            worker = self.select_build_worker(vg.position) #  Need to delegate worker to build REFINERY as locations are non-random
             if worker is None:
                 break
             if not self.units(REFINERY).closer_than(1.0, vg).exists:
                 await self.do(worker.build(REFINERY, vg))
-
+                
+    # build BARRACKS to build MARINE if we can afford a BARRACKS and the amount of BARRACKS is less than 2
     async def build_barracks(self):
         if self.can_afford(BARRACKS) and self.units(BARRACKS).amount < 2:
-            await self.build(BARRACKS, near=self.units(COMMANDCENTER).first)
+            await self.build(BARRACKS, near=self.units(COMMANDCENTER).first)  # position BARRACKS next to first COMMANDCENTER
 
+    # for each BARRACKS build MARINE if we can afford a MARINE and the amount of MARINE is smaller than 40
     async def build_marines(self):
         for rax in self.units(BARRACKS):
             if rax.is_ready and rax.noqueue:
                 if self.can_afford(MARINE) and self.units(MARINE).amount < 40:
                     await self.do(rax.train(MARINE))
-
+                    
+    # build FACTORY to build CYCLONE if amount of BARRACKS is larger than 1 and we can afford a FACTORY and we have no more than 1 FACTORY
     async def build_factory(self):
         if self.units(BARRACKS).amount > 0:
             if self.can_afford(FACTORY) and self.units(FACTORY).amount < 1 and not self.already_pending(FACTORY):
-                await self.build(FACTORY, near=self.units(BARRACKS).first)
-
+                await self.build(FACTORY, near=self.units(BARRACKS).first)  # position FACTORY next to first BARRACKS
+    
+    # for each FACTORY build CYCLONE if we can afford a CYCLONE and the amount of CYCLONE is smaller than 12
     async def build_cyclones(self):
         for fact in self.units(FACTORY):
             if fact.is_ready and fact.noqueue:
                 if self.can_afford(CYCLONE) and self.units(CYCLONE).amount < 12:
                     await self.do(fact.train(CYCLONE))
-
+    
+    # function to identify the target of our attack
     def locate_target(self, state):
-        if len(self.known_enemy_units) > 0:
-            return random.choice(self.known_enemy_units)
+        if len(self.known_enemy_units) > 0:  
+            return random.choice(self.known_enemy_units)  # if we know of more than 0 enemy units, there are our target
         elif len(self.known_enemy_structures) > 0:
-            return random.choice(self.known_enemy_structures)
+            return random.choice(self.known_enemy_structures) # elif we know of more than 0 enemy buildings, there are our target
         else:
-            return self.enemy_start_locations[0]
-
+            return self.enemy_start_locations[0]  # else, the enemy start location is our target
+    
+    # attack enemy once min 20 MARINE are built and min 4 CYCLONE are built
     async def attack(self):
+        # uncommented to break up second push into smaller MARINE segments
         # if self.units(MARINE).amount > 5:
         #     if len(self.known_enemy_units) > 0:
         #         for m in self.units(MARINE).idle:
         #             await self.do(m.attack(random.choice(self.known_enemy_units)))
 
-        if self.units(MARINE).amount > 20 and self.units(CYCLONE).amount > 4:
+        if self.units(MARINE).amount > 20 and self.units(CYCLONE).amount > 4:  
             for m in self.units(MARINE).idle:
                 await self.do(m.attack(self.locate_target(self.state)))
             for c in self.units(CYCLONE).idle:
